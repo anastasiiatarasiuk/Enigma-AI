3b9cf45924b312426533c86f5fd1e9c072843b33
Anastasiia Tarasiuk <111896564+anastasiiatarasiuk@users.noreply.github.com>
2024-12-17 14:05:26 +0200

    create Pro page

--- src/pages/ProPage/ProPage.jsx
+++ src/pages/ProPage/ProPage.jsx
@@ -1,5 +1,22 @@
+import FAQ from "../../components/FAQ/FAQ";
+import Price from "../../components/Price/Price";
+import TakeOff from "../../components/TakeOff/TakeOff";
+import s from "./ProPage.module.css";
+
 const ProPage = () => {
-  return <div>ProPage</div>;
+  return (
+    <>
+      <section className={s.bg}>
+        <Price />
+      </section>
+      <section>
+        <FAQ />
+      </section>
+      <section>
+        <TakeOff />
+      </section>
+    </>
+  );
 };
 
 export default ProPage;
